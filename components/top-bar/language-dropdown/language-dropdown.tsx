cf35dc54fc7677e5c96ee34efec064e7d2fd0005
tf63 <74246282+tf63@users.noreply.github.com>
2024-06-23 19:58:14 +0900

    impl language dropdown

---
+++ components/top-bar/language-dropdown/language-dropdown.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { useNextCode } from '@/components/hooks/use-next-code'
+
+export const LanguageDropdown = () => {
+    const { nextCode } = useNextCode()
+
+    return (
+        <div className="dropdown w-80" defaultValue={'typescript'}>
+            <div tabIndex={0} role="button" className="btn w-full justify-between">
+                Language
+                <svg
+                    width="12px"
+                    height="12px"
+                    className="inline-block h-2 w-2 fill-current opacity-60"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 2048 2048"
+                >
+                    <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+                </svg>
+            </div>
+            <ul tabIndex={0} className="dropdown-content z-[1] mt-2 w-full rounded-box bg-base-300 p-2 shadow-2xl">
+                <li>
+                    <input
+                        type="radio"
+                        name="language-dropdown"
+                        className="btn btn-ghost btn-sm btn-block justify-start"
+                        aria-label="TypeScript"
+                        value="typescript"
+                        onChange={() => {
+                            nextCode()
+                        }}
+                    />
+                </li>
+                <li>
+                    <input
+                        type="radio"
+                        name="language-dropdown"
+                        className="btn btn-ghost btn-sm btn-block justify-start"
+                        aria-label="Go"
+                        value="go"
+                        disabled
+                    />
+                </li>
+                <li>
+                    <input
+                        type="radio"
+                        name="language-dropdown"
+                        className="btn btn-ghost btn-sm btn-block justify-start"
+                        aria-label="Python"
+                        value="python"
+                        disabled
+                    />
+                </li>
+            </ul>
+        </div>
+    )
+}
--- components/top-bar/top-bar.tsx
+++ components/top-bar/top-bar.tsx
@@ -1,4 +1,4 @@
-import { LanguageSelect } from './language-select'
+import { LanguageDropdown } from './language-dropdown'
 import { NextCodeButton } from './next-code-button'
 
 export const TopBar = () => {
@@ -6,7 +6,7 @@ export const TopBar = () => {
         <div className="flex items-center">
             <div className="px-4 py-3 text-center text-lg text-white">Type App</div>
             <div className="ml-auto flex items-center gap-10">
-                <LanguageSelect />
+                <LanguageDropdown />
                 <NextCodeButton />
             </div>
         </div>
