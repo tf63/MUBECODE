68e67618d2cebbfacd1c4324d4e41df4a3034066
tf63 <74246282+tf63@users.noreply.github.com>
2024-06-24 09:07:46 +0900

    impl theme store

--- components/top-bar/theme-select/theme-select.tsx
+++ components/top-bar/theme-select/theme-select.tsx
@@ -1,6 +1,15 @@
+'use client'
 import { Palette } from 'lucide-react'
 
+import { useThemeStore } from '@/components/store/theme-store'
+
 export const ThemeSelect = () => {
+    const { setTheme } = useThemeStore()
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTheme(event.target.value)
+    }
+
     return (
         <div className="flex items-start">
             <Palette className="mr-3 h-8 w-8" />
@@ -10,6 +19,7 @@ export const ThemeSelect = () => {
                     name="theme"
                     value="default"
                     className="theme-controller radio radio-lg  checked:bg-primary"
+                    onChange={handleChange}
                     defaultChecked
                 />
                 <input
@@ -17,18 +27,21 @@ export const ThemeSelect = () => {
                     name="theme"
                     value="dracula"
                     className="theme-controller radio  radio-lg checked:bg-primary"
+                    onChange={handleChange}
                 />
                 <input
                     type="radio"
                     name="theme"
                     value="sunset"
                     className="theme-controller radio radio-lg  checked:bg-primary"
+                    onChange={handleChange}
                 />
                 <input
                     type="radio"
                     name="theme"
                     value="night"
                     className="theme-controller radio radio-lg  checked:bg-primary"
+                    onChange={handleChange}
                 />
             </div>
         </div>
