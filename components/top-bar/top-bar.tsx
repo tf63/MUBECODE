172ba133b6f54fc1e1b6a318afa993b37a908a68
tf63 <74246282+tf63@users.noreply.github.com>
2024-06-27 22:58:29 +0900

    add icon & title

--- components/top-bar/top-bar.tsx
+++ components/top-bar/top-bar.tsx
@@ -5,11 +5,10 @@ import { ThemeSelect } from './theme-select'
 export const TopBar = () => {
     return (
         <div className="flex items-center">
-            <div className="px-4 py-3 text-center text-lg text-white">Type App</div>
+            <div className="px-4 py-3 text-center text-lg text-white">MUBECODE</div>
             <div className="ml-auto flex items-center gap-10">
                 <ThemeSelect />
                 <LanguageDropdown />
-           
                 <NextCodeButton />
             </div>
         </div>
