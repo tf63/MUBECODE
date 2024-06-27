729e91bf915d78588cd9b488efbe9fa2dd74f97c
tf63 <74246282+tf63@users.noreply.github.com>
2024-06-27 13:46:19 +0900

    impl localstorage

--- components/top-bar/theme-select/theme-select.tsx
+++ components/top-bar/theme-select/theme-select.tsx
@@ -1,48 +1,51 @@
 'use client'
 import { Palette } from 'lucide-react'
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
 
 import { useThemeStore } from '@/components/store/theme-store'
 
-export const ThemeSelect = () => {
-    const { setTheme } = useThemeStore()
+const ThemeRadioButton = ({ label }: { label: Theme }) => {
+    const { theme, setTheme } = useThemeStore()
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTheme(event.target.value)
-    }
+    useEffect(() => {
+        const curTheme = localStorage.getItem('theme') as Theme
+        if (curTheme == null) {
+            localStorage.setItem('theme', 'default')
+        }
+
+        setTheme(curTheme ?? 'default')
+    }, [setTheme])
+
+    return (
+        <input
+            type="radio"
+            name="theme"
+            value={label}
+            data-set-theme={label}
+            data-act-class="ACTIVECLASS"
+            className="theme-controller radio radio-lg checked:bg-primary focus-visible:outline-0"
+            onChange={() => {
+                setTheme(label)
+            }}
+            checked={theme === label}
+        />
+    )
+}
+
+export const ThemeSelect = () => {
+    useEffect(() => {
+        themeChange(false)
+    }, [])
 
     return (
         <div className="flex items-start">
             <Palette className="mr-3 h-8 w-8" />
             <div className="space-x-2">
-                <input
-                    type="radio"
-                    name="theme"
-                    value="default"
-                    className="theme-controller radio radio-lg  checked:bg-primary focus-visible:outline-0"
-                    onChange={handleChange}
-                    defaultChecked
-                />
-                <input
-                    type="radio"
-                    name="theme"
-                    value="dracula"
-                    className="theme-controller radio  radio-lg checked:bg-primary focus-visible:outline-0"
-                    onChange={handleChange}
-                />
-                <input
-                    type="radio"
-                    name="theme"
-                    value="sunset"
-                    className="theme-controller radio radio-lg  checked:bg-primary focus-visible:outline-0"
-                    onChange={handleChange}
-                />
-                <input
-                    type="radio"
-                    name="theme"
-                    value="dim"
-                    className="theme-controller radio radio-lg  checked:bg-primary focus-visible:outline-0"
-                    onChange={handleChange}
-                />
+                <ThemeRadioButton label="default" />
+                <ThemeRadioButton label="dracula" />
+                <ThemeRadioButton label="sunset" />
+                <ThemeRadioButton label="dim" />
             </div>
         </div>
     )
