106fda54340a7f5c9a3f8ec3a22f6afed6407eb9
tf63 <74246282+tf63@users.noreply.github.com>
2024-06-27 22:52:58 +0900

    add other language

--- lib/hljs.ts
+++ lib/hljs.ts
@@ -1,6 +1,10 @@
 import hljs from 'highlight.js/lib/core'
+import go from 'highlight.js/lib/languages/go'
+import python from 'highlight.js/lib/languages/python'
 import typescript from 'highlight.js/lib/languages/typescript'
 hljs.registerLanguage('typescript', typescript)
+hljs.registerLanguage('go', go)
+hljs.registerLanguage('python', python)
 // import 'highlight.js/styles/agate.css'
 import 'highlight.js/styles/nord.css'
 // import 'highlight.js/styles/tokyo-night-dark.css'
