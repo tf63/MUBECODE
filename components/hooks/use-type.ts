d50029a7ff00b4f8a81bf4e4eb93532a5b379a8b
tf63 <74246282+tf63@users.noreply.github.com>
2024-06-30 15:35:32 +0900

    suppress rendering

--- components/hooks/use-type.ts
+++ components/hooks/use-type.ts
@@ -1,48 +1,38 @@
 import { useCallback, useEffect, useMemo } from 'react'
+import { useShallow } from 'zustand/react/shallow'
 
 import { useTypeStore } from '@/components/store/type-store'
 
-import { stripCode } from '@/lib/utils'
-
 const disableKeys = ['Shift', 'Control', 'Alt', 'Meta', 'CapsLock', 'Tab', 'Super']
 
-export const useType = (code: Code) => {
-    const state = useTypeStore()
+export const useCursorIndex = (line: string) => {
+    const [cursorIndex, isLineFinished, setLineFinished, incCursorIndex] = useTypeStore(
+        useShallow((state) => [state.cursorIndex, state.isLineFinished, state.setLineFinished, state.incCursorIndex])
+    )
 
-    const targetCode = useMemo(() => stripCode(code), [code])
+    const targetLine = useMemo(() => line.trim(), [line])
+
+    useEffect(() => {
+        if (cursorIndex === targetLine.length) {
+            setLineFinished(true)
+        }
+    }, [cursorIndex, targetLine.length, setLineFinished])
 
     const downHandler = useCallback(
         (event: KeyboardEvent) => {
             event.preventDefault()
 
             const key = event.key
-            const targetLine = targetCode[state.lineNumber].line
-            const targetKey = targetLine[state.cursorIndex]
+            const targetKey = targetLine[cursorIndex]
             if (disableKeys.includes(key)) return
-
-            if (state.cursorIndex === targetLine.length) {
-                if (key === 'Enter') {
-                    if (state.lineNumber === targetCode.length - 1) {
-                        // finish
-                        state.resetLineNumber()
-                        state.resetCursorIndex()
-                        return
-                    } else {
-                        // correct -> next line
-                        state.resetCursorIndex()
-                        state.incLineNumber()
-                        return
-                    }
-                }
-            }
+            if (isLineFinished) return
 
             if (key === targetKey) {
                 // correct -> next key
-                state.incCursorIndex()
-                return
+                incCursorIndex()
             }
         },
-        [state, targetCode]
+        [cursorIndex, targetLine, isLineFinished, incCursorIndex]
     )
 
     const upHandler = useCallback((event: KeyboardEvent) => {
@@ -59,9 +49,54 @@ export const useType = (code: Code) => {
         return () => controller.abort()
     }, [downHandler, upHandler])
 
-    if (code.length === 0) {
-        return { cursorIndex: 0, lineNumber: 0 }
-    }
+    return { cursorIndex }
+}
 
-    return { cursorIndex: state.cursorIndex, lineNumber: state.lineNumber }
+export const useLineNumber = (maxLineNumber: number) => {
+    const [lineNumber, isLineFinished, resetLineNumber, resetCursorIndex, incLineNumber] = useTypeStore(
+        useShallow((state) => [
+            state.lineNumber,
+            state.isLineFinished,
+            state.resetLineNumber,
+            state.resetCursorIndex,
+            state.incLineNumber,
+        ])
+    )
+
+    const downHandler = useCallback(
+        (event: KeyboardEvent) => {
+            event.preventDefault()
+
+            if (isLineFinished && event.key === 'Enter') {
+                if (lineNumber === maxLineNumber - 1) {
+                    // finish
+                    resetLineNumber()
+                    resetCursorIndex()
+                    return
+                } else {
+                    // correct -> next line
+                    resetCursorIndex()
+                    incLineNumber()
+                    return
+                }
+            }
+        },
+        [maxLineNumber, isLineFinished, lineNumber, resetCursorIndex, resetLineNumber, incLineNumber]
+    )
+
+    const upHandler = useCallback((event: KeyboardEvent) => {
+        event.preventDefault()
+    }, [])
+
+    useEffect(() => {
+        const controller = new AbortController()
+        const { signal } = controller
+
+        window.addEventListener('keydown', downHandler, { signal })
+        window.addEventListener('keyup', upHandler, { signal })
+
+        return () => controller.abort()
+    }, [downHandler, upHandler])
+
+    return { lineNumber }
 }
--- components/store/type-store.ts
+++ components/store/type-store.ts
@@ -3,8 +3,10 @@ import { create } from 'zustand'
 interface TypeStore {
     lineNumber: number
     cursorIndex: number
+    isLineFinished: boolean
     resetLineNumber: () => void
     resetCursorIndex: () => void
+    setLineFinished: (isLineFinished: boolean) => void
     incLineNumber: () => void
     incCursorIndex: () => void
 }
@@ -12,8 +14,10 @@ interface TypeStore {
 export const useTypeStore = create<TypeStore>((set) => ({
     lineNumber: 0,
     cursorIndex: 0,
+    isLineFinished: false,
     resetLineNumber: () => set({ lineNumber: 0 }),
     resetCursorIndex: () => set({ cursorIndex: 0 }),
-    incLineNumber: () => set((state) => ({ lineNumber: state.lineNumber + 1 })),
+    setLineFinished: (isLineFinished: boolean) => set({ isLineFinished: isLineFinished }),
+    incLineNumber: () => set((state) => ({ lineNumber: state.lineNumber + 1, isLineFinished: false })),
     incCursorIndex: () => set((state) => ({ cursorIndex: state.cursorIndex + 1 })),
 }))
